3bfd03fce545d4613d232b5020126bcdb82e40c8
Viktor <kluckmucki@gmail.com>
2017-09-13 20:49:43 +0900

    viktor

---
+++ prime5to50.py
@@ -0,0 +1,33 @@
+var = 0
+
+for var in range(50):
+    if var == 2:
+        print var
+    if var == 3:
+        print var
+    if var == 5:
+        print var
+    if var == 7:
+        print var
+    if var == 11:
+        print var
+    if var == 13:
+        print var
+    if var == 17:
+        print var
+    if var == 19:
+        print var
+    if var == 23:
+        print var
+    if var == 29:
+        print var
+    if var == 31:
+        print var
+    if var == 37:
+        print var
+    if var == 41:
+        print var
+    if var == 43:
+        print var
+    if var == 47:
+        print var
--- prime5to50.ts
+++ prime5to50.ts
@@ -1,9 +1,61 @@
 function prime5to50(): string {
-	let message : string = ""
-
-	// TODO: fill up the message string with the proper content.
-
-	return message
+	let message : string = "prime"
+	console.log("hello")
+	// TODO: fill up the message string with the proper content
+	var n:number = 51;
+	for(var i = 5; i < n; i++) {
+		if(i = 2) {
+			console.log(i);
+		}
+		if(i = 3) {
+			console.log(i);
+		}
+		if(i = 5) {
+			console.log(i);
+		}
+		if(i = 7) {
+			console.log(i);
+		}
+		if(i = 11) {
+			console.log(i);
+		}
+		if(i = 13) {
+			console.log(i);
+		}
+		if(i = 17) {
+			console.log(i);
+		}
+		if(i = 19) {
+			console.log(i);
+		}
+		if(i = 23) {
+			console.log(i);
+		}
+		if(i = 29) {
+			console.log(i);
+		}
+		if(i = 31) {
+			console.log(i);
+		}
+		if(i = 37) {
+			console.log(i);
+		}
+		if(i = 41) {
+			console.log(i);
+		}
+		if(i = 43) {
+			console.log(i);
+		}
+		if(i = 47) {
+			console.log(i);
+		}
+		/*for(var y = 2; y < i; y++) {
+			/if(i%y==0) {
+				console.log(i);
+			}
+		}*/
+		return message
+	}
 }
 
 // You may use additional helper functions.
