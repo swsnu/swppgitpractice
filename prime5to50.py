d38ad3084b859b353e595b3501c36b5409bc7a69
skystar-p <wogus150@naver.com>
2017-09-13 19:05:10 +0900

    implement prime number printer in python

---
+++ prime5to50.py
@@ -0,0 +1,8 @@
+for i in range(5, 51):
+    is_prime = True
+    for j in range(2, i):
+        if i % j == 0:
+            is_prime = False
+            break
+    if is_prime:
+        print('{} is a prime number'.format(i))
