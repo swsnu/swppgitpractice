fd5b7c7ccaeaaada936e4bc6ef4c249fe754f4b1
lucetre <lucetre@naver.com>
2017-09-13 19:53:54 +0900

    add prime typescript

--- prime5to50.ts
+++ prime5to50.ts
@@ -1,8 +1,17 @@
 function prime5to50(): string {
 	let message : string = ""
 
-	// TODO: fill up the message string with the proper content.
-
+	for (var n = 5; n < 50; n++) {
+		var isPrime = true;
+		for (var i = 2; i < n; i++) {
+			if (n % i == 0) {
+				isPrime = false;
+			}
+		}
+		if (isPrime) {
+			message += n + " is a prime number<br>";
+		}
+	}	
 	return message
 }
 
