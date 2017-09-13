e07e15cdae7167e127dd25ac521e29beda201834
Pimon <pimon@MacBook-Pro-khxng-Pimon.local>
2017-09-14 00:17:24 +0900

    add & edit prime5to50

--- prime5to50.ts
+++ prime5to50.ts
@@ -2,10 +2,19 @@ function prime5to50(): string {
 	let message : string = ""
 
 	// TODO: fill up the message string with the proper content.
-
+        for(var i = 5; i<=50; i++){
+           if(isPrime(i))
+              message += (i + " is a prime number<br/>");
+        }
 	return message
 }
 
 // You may use additional helper functions.
-
+function isPrime(i) {
+   for(var n = 2; n <= i-1; ++n) {
+      if(i % n==0)
+         return false;
+      }
+   return true;
+}
 document.body.innerHTML = prime5to50()
