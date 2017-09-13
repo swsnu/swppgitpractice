b873feb1632f358f1f3bb47e8f00ec8af4cec555
edwardycl <youngchan1115@gmail.com>
2017-09-13 21:07:26 +0900

    prime5to50.py added & prime5to50.ts modified

--- prime5to50.ts
+++ prime5to50.ts
@@ -2,7 +2,18 @@ function prime5to50(): string {
 	let message : string = ""
 
 	// TODO: fill up the message string with the proper content.
-
+	for(var i=5;i<=50;i++) {
+		var is_prime = true;
+		for(var j=2;j<i;j++) {
+			if(i%j == 0) {
+				is_prime = false;
+				break;
+			}
+		}
+		if(is_prime) {
+			message += i + " is a prime number<br>"; 
+		}
+	}
 	return message
 }
 
