b1f4f658458c6f1032168945271080d3b98fb8b9
root <96lives@gmail.com>
2017-09-13 19:29:22 +0900

    need to ass newline

--- prime5to50.ts
+++ prime5to50.ts
@@ -1,9 +1,22 @@
+
+function isPrime(n: number): boolean {
+    for (let i: number = 2; i < n; ++i) {
+        if (n % 2 == 0)
+            return false;
+    }
+    return true;
+}
+
 function prime5to50(): string {
-	let message : string = ""
+    let message : string = "";
+    for (let i: number = 5; i <= 50; ++i) {
+        if (isPrime(i))
+            message = message + i + " is a prime number\n";
+    }
 
-	// TODO: fill up the message string with the proper content.
+    // TODO: fill up the message string with the proper content.
 
-	return message
+    return message;
 }
 
 // You may use additional helper functions.
