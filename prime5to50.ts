3d79052e4c213197a46d48268408d6af484d0076
cloudytheconqueror <cloudy5939@gmail.com>
2017-09-13 19:53:05 +0900

    add prime5to50.ts

--- prime5to50.ts
+++ prime5to50.ts
@@ -2,6 +2,17 @@ function prime5to50(): string {
 	let message : string = ""
 
 	// TODO: fill up the message string with the proper content.
+	var i:number;
+	var j:number;
+
+	for(i = 5; i <= 50; i++) {
+		for(j = 2; j < i/2; j++) {
+			if (i % j == 0) break;
+		}
+		if(j >= i/2) {
+			message += "<p>" + i + " is a prime number</p>";
+		}
+	}
 
 	return message
 }
