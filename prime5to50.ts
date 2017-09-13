9fa2bab13f8ed80b83ebcce7e8c5e6fedf6b0fd5
KimHyeonwoo <woo960131@naver.com>
2017-09-13 19:53:34 +0900

    Implement prime5to50 in python and typescript

--- prime5to50.ts
+++ prime5to50.ts
@@ -1,11 +1,29 @@
 function prime5to50(): string {
-	let message : string = ""
+	let message : string = "";
 
 	// TODO: fill up the message string with the proper content.
 
-	return message
+  let n : number;
+
+  for (n = 5; n <= 50; n++) {
+    if (isPrime(n)) {
+    message = message + String(n) + " is a prime number<br>";
+    }
+  }
+
+	return message;
 }
 
 // You may use additional helper functions.
+function isPrime(n : number) : Boolean {
+  let a : number;
+  for (a = 2; a <= n; a++) {
+    if (n % a == 0) {
+      break;
+    }
+  }
+  if (n == a) return true;
+  else return false;
+}
 
-document.body.innerHTML = prime5to50()
+document.body.innerHTML = prime5to50();
