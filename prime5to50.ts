fb8c96d20105bce629a0681b1958a4de3a6f624f
yeonwooKim <nokjiwon96@gmail.com>
2017-09-13 19:53:41 +0900

    Write prime5to50.ts and .py

--- prime5to50.ts
+++ prime5to50.ts
@@ -1,7 +1,25 @@
 function prime5to50(): string {
-	let message : string = ""
+	let message : string = "";
 
-	// TODO: fill up the message string with the proper content.
+    let num : number = 5;
+    let flag: number = 0;
+
+    while (num <= 50) {
+        let div : number = 2;
+        while (div < num) {
+            if (num % div == 0) {
+                flag = 1;
+                break;
+            }
+            div = div + 1;
+        }
+        if (flag == 0) {
+            message = message.concat(num.toString());
+            message = message.concat(" is a prime number<br>");
+        }
+        flag = 0;
+        num = num + 1;
+    }
 
 	return message
 }
