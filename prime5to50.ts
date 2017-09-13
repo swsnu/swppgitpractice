c80bf9454db37fbf0dd357168169eaf6e9b581d5
cowgirl07 <cowgirl07@snu.ac.kr>
2017-09-13 21:41:44 +0900

    modified prime 5 to 50 both .py and .ts

--- prime5to50.ts
+++ prime5to50.ts
@@ -1,11 +1,27 @@
-function prime5to50(): string {
-	let message : string = ""
-
-	// TODO: fill up the message string with the proper content.
-
-	return message
+function prime5to50() {
+    var message = "";
+    // TODO: fill up the message string with the proper content.
+    var x;
+    var y;
+    var z;
+    var w;
+    var isPrime;
+    for (x = 50; x >= 5; x--) {
+        w = 55 - x;
+        isPrime = true;
+        for (y = w - 2; y >= 1; y--) {
+            z = w - y;
+            if (w % z == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        if (isPrime) {
+            message += w;
+            message += " is a prime<br>";
+        }
+    }
+    return message;
 }
-
 // You may use additional helper functions.
-
-document.body.innerHTML = prime5to50()
+document.body.innerHTML = prime5to50();
