3a7c31ae96904ff114754c0de1d699bc65bbabe0
sunki <sunki@snu.ac.kr>
2017-09-13 20:00:14 +0900

    add prim5to50.py and prime5to50.ts

--- prime5to50.ts
+++ prime5to50.ts
@@ -2,7 +2,17 @@ function prime5to50(): string {
 	let message : string = ""
 
 	// TODO: fill up the message string with the proper content.
-
+	for (let i = 5 ; i <= 50 ; i++ ){
+        if (i % 2 != 0){
+            if (i % 3 != 0){
+                if (i % 5 != 0 || i == 5 ){
+                    if (i % 7 != 0 || i == 7 ){
+                        message += `${i} is prime number</br>`;
+                    }
+                }
+            }
+        }
+	}
 	return message
 }
 
