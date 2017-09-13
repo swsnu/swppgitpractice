80ccb462f97a05e687b9be108cdb474ae8882659
Nicholas Chung <nkchung@ucsd.edu>
2017-09-13 20:24:07 +0900

    add prime programs

--- prime5to50.ts
+++ prime5to50.ts
@@ -2,7 +2,23 @@ function prime5to50(): string {
 	let message : string = ""
 
 	// TODO: fill up the message string with the proper content.
-
+	var num:number;
+	var i:number;
+        var isPrime: boolean;
+	for(num=5;num<=50;num++) {
+	  if (num > 1) {
+            isPrime = true;
+	    for(i=2;i<num;i++) {
+	      if ( (num % i) == 0) {
+                isPrime = false;
+	        break;
+	      }
+	    }
+            if (isPrime) {
+              message = message + num + " is a prime number <br>";
+	    }
+          }
+	}
 	return message
 }
 
