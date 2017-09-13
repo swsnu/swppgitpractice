f5e92a1bd867906f1d39d3dd39ca0ab1d0c68183
jhbaang <apooapooar@gmail.com>
2017-09-13 20:04:25 +0900

    create prime printing typescript file

--- prime5to50.ts
+++ prime5to50.ts
@@ -1,5 +1,14 @@
+function isprime(num: number): boolean {
+	for(let i: number = 2; i < num ; ++i){
+		if(num%i == 0) return false;
+	}
+	return true;
+}
 function prime5to50(): string {
 	let message : string = ""
+	for(let i: number=5 ; i <= 50 ; ++i){
+		if(isprime(i)) message = message + i + " is a prime number" + "<br/>"
+	}
 
 	// TODO: fill up the message string with the proper content.
 
