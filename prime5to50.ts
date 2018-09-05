5442d4584d745119af7fb42b6c729efcd126b51d
donguk <uk6342@gmail.com>
2018-09-05 19:50:07 +0900

    implement prime5to50

--- prime5to50.ts
+++ prime5to50.ts
@@ -1,11 +1,20 @@
 function prime5to50(): string {
 	let message : string = ""
 
-	// TODO: fill up the message string with the proper content.
+  for(let num:number= 5; num <= 50; num = num + 1){
+    let isPrime:boolean= true;
+    for(let i:number= 2; i < num ; i = i + 1){
+      if(num % i == 0){
+        isPrime = false;
+        break;
+      }
+    }
+    if(isPrime){
+      message += num + " is a prime \n";
+    }
+  }
 
 	return message
 }
-
-// You may use additional helper functions.
 
 document.body.innerHTML = prime5to50()
