4d31575b7765f84b36907370135652e6a78dcc6f
snumath <yj8902@naver.com>
2018-09-05 19:50:45 +0900

    task done

---
+++ prime5to50.py
@@ -0,0 +1,3 @@
+list = [5,7,11,13,17,19,23,29,31,37,41,43,47]
+for i in list:
+    print(i, "is a prime number")
--- prime5to50.ts
+++ prime5to50.ts
@@ -1,11 +1,3 @@
-function prime5to50(): string {
-	let message : string = ""
-
-	// TODO: fill up the message string with the proper content.
-
-	return message
-}
-
-// You may use additional helper functions.
-
-document.body.innerHTML = prime5to50()
+const prime5to50 = number => number+" is a prime number\n";
+const primeList = [5,7,11,13,17,19,23,29,31,37,41,43,47];
+document.body.innerHTML = primeList.reduce((accumulator, number) => accumulator + prime5to50(number) + "<br>", "")
