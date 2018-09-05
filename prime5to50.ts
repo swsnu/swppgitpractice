60426bb23edde946f93e14d31a972a542fa4b1d5
insung151 <insung151@naver.com>
2018-09-05 19:55:57 +0900

    Add prime5to50

--- prime5to50.ts
+++ prime5to50.ts
@@ -1,11 +1,22 @@
+function is_prime(x) {
+	for (var i=3; i<x; i++){
+		if (x % i == 0) {
+            return false
+        }
+	}
+	return true
+}
 function prime5to50(): string {
-	let message : string = ""
+	let message : string = "";
+	for (var i=5; i<=50; i++){
+		if (is_prime(i))
+			message += i + " is a prime number<br/>";
+	}
 
-	// TODO: fill up the message string with the proper content.
 
 	return message
 }
 
 // You may use additional helper functions.
 
-document.body.innerHTML = prime5to50()
+document.body.innerHTML = prime5to50();
