a82649489d03a2fa8963cbb2891497995914b17f
Kim Woojung <kimwj317@naver.com>
2018-09-05 20:47:05 +0900

    adfsa

--- prime5to50.ts
+++ prime5to50.ts
@@ -1,11 +1,16 @@
-function prime5to50(): string {
-	let message : string = ""
+var i:number;
+var j:number;
+var zz:boolean;
 
-	// TODO: fill up the message string with the proper content.
-
-	return message
+for(i=5;i<=50;i++){
+	zz=true;
+	for(j=2; j<i; j++){
+		if(i%j == 0){
+			zz=false;
+			break;
+		}
+		}
+		if(zz==true){
+		console.log(i+" is a prime number");
+	}
 }
-
-// You may use additional helper functions.
-
-document.body.innerHTML = prime5to50()
