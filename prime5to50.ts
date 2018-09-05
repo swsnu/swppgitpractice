bbb263cf2f11cbf100e599d21a77d41d6aa5c288
MountGuy <riman314@snu.ac.kr>
2018-09-05 19:55:39 +0900

    add prime5to50

--- prime5to50.ts
+++ prime5to50.ts
@@ -1,6 +1,18 @@
 function prime5to50(): string {
 	let message : string = ""
 
+
+		var i:number;
+		for(i = 5; i <= 50; i++)
+		{
+				var j:number;
+				for(j = 2; i%j != 0; j++)
+				{
+				}
+			if(j == n)
+				console.log(i + " is a prime number");
+
+		}
 	// TODO: fill up the message string with the proper content.
 
 	return message
