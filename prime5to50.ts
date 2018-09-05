1acf2df440dc1744ad1f3b94758a2ad456c4c20a
Do Il Yoon <ttotto3080@gmail.com>
2018-09-05 19:17:14 +0900

    implement prime5to50

--- prime5to50.ts
+++ prime5to50.ts
@@ -1,7 +1,22 @@
+function isPrime(num: number) {
+	for(let i = 2; i <= num; i++) {
+		if(num % i == 0) {
+			if(i == num) return true;
+			else return false;
+		}
+	}
+	return true;
+}
+
 function prime5to50(): string {
 	let message : string = ""
 
 	// TODO: fill up the message string with the proper content.
+	for(let i = 5; i <= 50; i++) {
+		if(isPrime(i)) {
+			message = message + `${i} is a prime number\n`;
+		}
+	}
 
 	return message
 }
