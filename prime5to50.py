c794072e65ebed2a267b3f87de9ad5129e2bf627
Park Ji Sang <jsp13@snu.ac.kr>
2018-09-05 19:50:05 +0900

    add prime5to50.py

---
+++ prime5to50.py
@@ -0,0 +1,7 @@
+for num in range(5, 50):
+    if num > 1:
+        for i in range(2,num):
+            if (num % i) == 0:
+                break
+        else:
+            print(num, "is a prime number")
