01e432f4ebe89789bf3b977b4cc06a34c2f723d1
Kim Jiwon <kimjiwon@Kims-MacBook-Pro.local>
2018-09-08 17:18:27 +0900

    Code changes in py, tsc file

--- prime5to50.ts
+++ prime5to50.ts
@@ -2,7 +2,21 @@ function prime5to50(): string {
 	let message : string = ""
 
 	// TODO: fill up the message string with the proper content.
+	let primeList: number[];
 
+	for (var i:number = 5; i<=50; i++) {
+		var check:number = 2;
+		while (check < i) {
+			if (i % check == 0) {
+				break;
+			} else {
+				check ++;
+			}
+		}
+		if (check == i) {
+			message += i + " is a prime number" + "<br/>";
+		}
+	}
 	return message
 }
 
