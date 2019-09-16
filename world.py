f042509eaab3c429c1134e4816c019326c5df30d
donghae <jangdonghae@snu.ac.kr>
2019-09-16 18:04:15 +0900

    git practice

---
+++ world.py
@@ -0,0 +1,13 @@
+#world.py file
+import itme
+
+def hello_world():
+	print("hello world")
+	
+def bye_world():
+	time.sleep(2)
+	print("bye world")
+	
+if __name__ == "__main__":
+	hello_wordl()
+	bye_world()
