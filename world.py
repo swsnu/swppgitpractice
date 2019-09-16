78df5deeea21345d248c5d3873b6d400a17203fa
Sungbin <qkr8545@gmail.com>
2019-09-16 18:09:52 +0900

    bye world after 2 secs

---
+++ world.py
@@ -0,0 +1,13 @@
+#world.py file
+import time
+
+def hello_world():
+    print("Hello world")
+
+def bye_world():
+    time.sleep(2)
+    pritn("bye world")
+
+if __name__ = "__main__":
+    hello_world()
+    bye_world()
