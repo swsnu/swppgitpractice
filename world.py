edaf1479512c9915d9f2a59458ec1c570a363d6c
root <root@swppfallhelloing.s0idiwzjqstedftqtlatiho0od.bx.internal.cloudapp.net>
2019-09-16 08:18:24 +0000

    Created world.py

---
+++ world.py
@@ -0,0 +1,13 @@
+#world.py file
+import time
+
+def hell_world():
+    print("hello world")
+
+def bye_world():
+    time.sleep(2)
+    print("hello world")
+
+if __name__ = "__main__":
+    hell_world()
+    bye_world()
